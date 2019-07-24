273b02bbcd3e666b57b9ee86cd214aeceb1d0c3c
michinobu.sekikawa <abc_0615@icloud.com>
2019-07-25 04:18:19 +0900

    RubyプログラムからMySQLとの通信手段を確保

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
+require 'mysql2'
 
 get '/' do
+   client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
+   @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
    erb :contact
 end
 
 post '/' do
+   client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
+   chouchou_name = params['name']
+   statement = client.prepare('INSERT INTO payments (name) VALUES(?)')
+   statement.execute(chouchou_name)
+
+   @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
    erb :complete
 end
