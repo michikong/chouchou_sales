ac597d5a0ae1075d3c7d1812d192f0a60f41ea8a
michinobu.sekikawa <abc_0615@icloud.com>
2019-07-26 21:09:13 +0900

    Gemfileの追加

--- app.rb
+++ app.rb
@@ -2,17 +2,24 @@ require 'sinatra'
 require 'mysql2'
 
 get '/' do
+   # mysqlに接続
+   # host、username、password、データベース名を指定
    client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
+   
+   # レコードの追加
    @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
    erb :contact
 end
-
+   
 post '/' do
+   # mysqlに接続
+   # host、username、password、データベース名を指定
    client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
-   chouchou_name = params['name']
-   statement = client.prepare('INSERT INTO payments (name) VALUES(?)')
-   statement.execute(chouchou_name)
-
+   form_typed = params['']
+   statement = client.prepare('INSERT INTO payments() VALUES()')
+   statement.execute(form_typed)
+   
+   # レコードの追加
    @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
    erb :complete
 end
