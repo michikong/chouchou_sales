6cb5f5da18a02e8c1215cee9522c474d85badf1b
michinobu.sekikawa <abc_0615@icloud.com>
2019-08-07 19:37:00 +0900

    編集

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
-require 'sinatra'
-require 'mysql2'
-require 'pry'
+# require 'sinatra'
+# require 'mysql2'
+# require 'pry'
 # binding.pry
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
 
 set :database, {adapter: "sqlite3", database: "contacts.sqlite3"}
 enable :sessions
@@ -14,23 +18,23 @@ end
 get '/' do
    # mysqlに接続
    # host、username、password、データベース名を指定
-   client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
+   # client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
    
    # レコードの追加
-   @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
+   # @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
    erb :contact
 end
    
 post '/' do
    # mysqlに接続
    # host、username、password、データベース名を指定
-   client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
+   # client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'chouchou', encoding: 'utf8')
    
    # parameter = params
-   statement = client.prepare('INSERT INTO payments(name, ruby, age, gender, payment, content) VALUES(?, ?, ?, ?, ?, ?)')
-   statement.execute(params['name'], params['ruby'], params['age'], params['gender'], params['payment'], params['content'])
+   # statement = client.prepare('INSERT INTO payments(name, ruby, age, gender, payment, content) VALUES(?, ?, ?, ?, ?, ?)')
+   # statement.execute(params['name'], params['ruby'], params['age'], params['gender'], params['payment'], params['content'])
    
-   # レコードの追加
-   @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
+   # # レコードの追加
+   # @records = client.query("SELECT * FROM payments ORDER BY created_at DESC")
    erb :complete
 end
