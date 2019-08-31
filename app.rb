eeca5ee26879d45c7814e81560826c2a49bde979
michinobu.sekikawa <abc_0615@icloud.com>
2019-08-31 21:32:12 +0900

    DBに保存できるようにに編集

--- app.rb
+++ app.rb
@@ -37,27 +37,13 @@ get '/' do
 end
 
 
+
 get '/payment' do
    @info = Info.new
    erb :payment
 end
 
-get '/company' do
-   erb :company
-end
-
-get '/address' do
-   erb :address
-end
-
 post '/payment' do
-
-   # name = params[:name]
-   # ruby = params[:ruby]
-   # age = params[:age]
-   # gender = params[:gender]
-   # payment = params[:payment]
-   # contact = params[:contact]
 
    @info = Info.new({
       name: params[:name],
@@ -91,18 +77,20 @@ post '/payment' do
    # erb :complete
 end
 
+get '/payment_aggregate_results' do
+   @info = Info.new
+   erb :payment_aggregate_results
+end
+
+
+
+
+
 get '/contact' do
    @contact = Contact.new
    @message = session.delete :message
    erb :contact
 end
-
-post '/contact_complete' do
-   @contact = Contact.new
-   @message = session.delete :message
-   erb :contact_complete
-end
-
 
 post '/contact' do
    
@@ -117,8 +105,8 @@ post '/contact' do
       confirm_email2: params[:confirm_email2]
    })
    
-   if params[:confirm_email1] + '@' + params[:confirm_email2] == params[:email]
-      @contact.save
+   # if params[:confirm_email1] + '@' + params[:confirm_email2] == params[:email]
+   if @contact.save
       erb :contact_complete
       # session[:message] = "#{name}さん"
       # redirect 'contact_complete'
@@ -129,17 +117,30 @@ post '/contact' do
    end
 end
 
-get '/blog' do
-   erb :blog
-end
-
-get '/payment_aggregate_results' do
-   @info = Info.new
-   erb :payment_aggregate_results
-end
-
 get '/contact_all' do
    @contact = Contact.new
    # binding.pry
    erb :contact_all
 end
+
+
+
+get '/blog' do
+   erb :blog
+end
+
+
+
+get '/company' do
+   erb :company
+end
+
+
+
+get '/address' do
+   erb :address
+end
+
+
+
+
